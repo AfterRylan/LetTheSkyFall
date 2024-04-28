f60f62da944fa99a355c01953e6c44226028667e
AfterRylan <168332740+AfterRylan@users.noreply.github.com>
2024-04-28 13:28:01 +0500

    Исправленный вектор
    теперь даже работает

---
+++ Vector.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	vector<int> vec;
+	for (int i = 0; i < 100; i++) {
+		vec.push_back(rand() % 100);
+	}
+
+	for (auto i : vec) {
+		cout << i << " ";
+	}
+	cout << endl;
+	sort(vec.begin(), vec.end());
+
+	for (auto i : vec) {
+		cout << i << " ";
+	}
+}
