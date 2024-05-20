72c955b97bf77736a857508247ba5760c1a4aa4a
AfterRylan <168332740+AfterRylan@users.noreply.github.com>
2024-05-20 20:20:20 +0500

    Задание

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+
+int main() {
+
+    setlocale(LC_ALL, "RUS");
+    int a;
+
+    std::cout << "Введите а:";
+    std::cin >> a;
+    for (int i(0); i < a; i++) {
+        for (int j(a); j != i; j--) {
+            std::cout << " ";
+        }
+        for (int r(0); r != i; r++) {
+            std::cout << "**";
+        }
+        std::cout << std::endl;
+    }
+    system("pause");
+    return 0;
+}
